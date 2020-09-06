58eab017ce73251f8a05915cde43cfe2d89911dc
Hazieq Riefdy <hazieqriefdy@gmail.com>
2020-09-07 01:22:24 +0800

    Home UI Fix

---
+++ src/ui_modules/Login.vue
@@ -0,0 +1,156 @@
+<template>
+<body>
+  <noscript>
+    <strong>We're sorry but zoa-front-next doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
+  </noscript>
+  <div data-app="true" class="v-application v-application--is-ltr theme--light" id="app">
+    <div class="v-application--wrap">
+      <!---->
+      <!---->
+      <!---->
+      <div role="dialog" class="v-dialog__container">
+        <!---->
+      </div>
+      <div
+        data-v-071bea94
+        class="v-card v-card--flat v-sheet v-sheet--tile theme--light"
+        style="height: 100vh; width: 100%; background-color: rgb(41, 98, 255); border-color: rgb(41, 98, 255);"
+      >
+        <div data-v-071bea94 class="layout fill-height align-center justify-center">
+          <div data-v-071bea94 class="flex text-xs-center md5 lg3 xl3">
+            <div data-v-071bea94 class="cursor pb-6 router-link-active" style="text-align: center;">
+              <img data-v-071bea94 src="/img/logo.a1fb7e1d.svg" />
+            </div>
+            <div data-v-071bea94 class="elevation-0 py-10 px-10 v-card v-sheet theme--light">
+              <div data-v-071bea94 class="v-toolbar__title">
+                <h3
+                  data-v-071bea94
+                  class="font-weight-medium header text-center pb-8"
+                >Log into your account</h3>
+              </div>
+              <div data-v-071bea94 class="v-card__text pa-0">
+                <form data-v-071bea94 novalidate="novalidate" class="v-form">
+                  <div data-v-071bea94 class="flex xs12">
+                    <h3 data-v-071bea94 class="text-left font-weight-medium py-2">Email</h3>
+                    <div
+                      data-v-071bea94
+                      class="v-input v-input--is-label-active v-input--is-dirty theme--light v-text-field v-text-field--is-booted v-text-field--enclosed v-text-field--outlined"
+                    >
+                      <div class="v-input__control">
+                        <div class="v-input__slot">
+                          <fieldset aria-hidden="true">
+                            <legend style="width: 0px;">
+                              <span>​</span>
+                            </legend>
+                          </fieldset>
+                          <div class="v-text-field__slot">
+                            <input name="email" required="required" id="input-14" type="email" />
+                          </div>
+                        </div>
+                        <div class="v-text-field__details">
+                          <div class="v-messages theme--light">
+                            <div class="v-messages__wrapper"></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div data-v-071bea94 class="flex xs12">
+                    <h3 data-v-071bea94 class="text-left font-weight-medium py-2">Password</h3>
+                    <div
+                      data-v-071bea94
+                      class="v-input v-input--is-label-active v-input--is-dirty theme--light v-text-field v-text-field--is-booted v-text-field--enclosed v-text-field--outlined"
+                    >
+                      <div class="v-input__control">
+                        <div class="v-input__slot">
+                          <fieldset aria-hidden="true">
+                            <legend style="width: 0px;">
+                              <span>​</span>
+                            </legend>
+                          </fieldset>
+                          <div class="v-text-field__slot">
+                            <input
+                              name="password"
+                              required="required"
+                              id="input-17"
+                              type="password"
+                            />
+                          </div>
+                          <div class="v-input__append-inner">
+                            <div class="v-input__icon v-input__icon--append">
+                              <button
+                                type="button"
+                                aria-label="append icon"
+                                class="v-icon notranslate v-icon--link mdi mdi-eye-off-outline theme--light"
+                              ></button>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="v-text-field__details">
+                          <div class="v-messages theme--light">
+                            <div class="v-messages__wrapper"></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <!---->
+                  </div>
+                  <div data-v-071bea94 class="flex xs12">
+                    <div align="center" data-v-071bea94 class="primary--text caption cursor pb-8">
+                      <button
+                        data-v-071bea94
+                        type="button"
+                        class="v-btn text v-btn--flat v-btn--text theme--light v-size--default px-0 center"
+                        style="text-transform: none !important;"
+                      >Forgot password</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+              <div data-v-071bea94 class="v-card__action">
+                <button
+                  class="v-btn v-btn--block v-btn--contained theme--light v-size--x-large elevation-0"
+                  type="button"
+                  data-v-071bea94
+                >Login</button>
+              </div>
+            </div>
+            <div data-v-071bea94 class="body-1 py-4 px-2 cursor" style="text-align: center;">
+              <button
+                data-v-071bea94
+                type="button"
+                class="white--text v-btn v-btn--flat v-btn--text theme--light v-size--default"
+                style="text-transform: none !important;"
+              >Sign up for free</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!---->
+    </div>
+  </div>
+  <script src="/js/chunk-vendors.988505d5.js"></script>
+  <script src="/js/app.6f751f69.js"></script>
+</body>
+</template>
+
+<style scoped>
+h3.text-left.font-weight-medium.py-2 {
+  color: darkslategrey;
+}
+
+button.v-btn.text.v-btn--flat.v-btn--text.theme--light.v-size--default {
+  color: #2962ff;
+}
+
+.v-btn--flat,
+.v-btn--outlined,
+.v-btn--text {
+  background-color: transparent;
+}
+
+button.v-btn.v-btn--block.v-btn--contained.theme--light.v-size--x-large {
+    color: white;
+    background: #2962FF;
+}
+</style>
