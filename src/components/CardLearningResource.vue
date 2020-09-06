2c1277ca709eafb07843bb189b2c82d05e11851c
Hazieq Riefdy <hazieqriefdy@gmail.com>
2020-09-07 02:54:21 +0800

    Card Resources

---
+++ src/components/CardLearningResource.vue
@@ -0,0 +1,90 @@
+<template>
+  <div
+    data-v-b9fde658
+    class="mt-4 v-card v-sheet theme--dark"
+    style="background-color: rgb(18, 34, 50); border-color: rgb(18, 34, 50);"
+  >
+    <div data-v-b9fde658 class="v-card__title px-5 pt-4 pb-0">Learning and resources</div>
+    <div
+      data-v-b9fde658
+      role="list"
+      class="v-list v-sheet v-sheet--tile theme--dark pb-4"
+      style="background-color: rgb(18, 34, 50);"
+    >
+      <div data-v-b9fde658 tabindex="-1" role="listitem" class="v-list-item theme--dark px-5">
+        <div
+          data-v-b9fde658
+          class="v-avatar v-list-item__avatar"
+          style="height: 40px; min-width: 40px; width: 40px;"
+        >
+          <i
+            data-v-b9fde658
+            aria-hidden="true"
+            class="v-icon notranslate mdi mdi-android theme--dark"
+            style="color: rgb(235, 87, 87); caret-color: rgb(235, 87, 87);"
+          ></i>
+        </div>
+        <div data-v-b9fde658 class="v-list-item__title">
+          <a data-v-b9fde658 class="btmBorderAnim2" style="color: inherit;">Start product tour</a>
+        </div>
+      </div>
+      <div data-v-b9fde658 tabindex="-1" role="listitem" class="v-list-item theme--dark px-5">
+        <div
+          data-v-b9fde658
+          class="v-avatar v-list-item__avatar"
+          style="height: 40px; min-width: 40px; width: 40px;"
+        >
+          <i
+            data-v-b9fde658
+            aria-hidden="true"
+            class="v-icon notranslate mdi mdi-play theme--dark"
+            style="color: rgb(39, 174, 96); caret-color: rgb(39, 174, 96);"
+          ></i>
+        </div>
+        <div data-v-b9fde658 class="v-list-item__title">
+          <a data-v-b9fde658 class="btmBorderAnim2" style="color: inherit;">Video tutorials</a>
+        </div>
+      </div>
+      <div data-v-b9fde658 tabindex="-1" role="listitem" class="v-list-item theme--dark px-5">
+        <div
+          data-v-b9fde658
+          class="v-avatar v-list-item__avatar"
+          style="height: 40px; min-width: 40px; width: 40px;"
+        >
+          <i
+            data-v-b9fde658
+            aria-hidden="true"
+            class="v-icon notranslate mdi mdi-gesture-tap theme--dark"
+            style="color: rgb(155, 81, 224); caret-color: rgb(155, 81, 224);"
+          ></i>
+        </div>
+        <div data-v-b9fde658 class="v-list-item__title">
+          <a data-v-b9fde658 class="btmBorderAnim2" style="color: inherit;">Getting started tips</a>
+        </div>
+      </div>
+      <div data-v-b9fde658 tabindex="-1" role="listitem" class="v-list-item theme--dark px-5">
+        <div
+          data-v-b9fde658
+          class="v-avatar v-list-item__avatar"
+          style="height: 40px; min-width: 40px; width: 40px;"
+        >
+          <i
+            data-v-b9fde658
+            aria-hidden="true"
+            class="v-icon notranslate mdi mdi-message-text theme--dark"
+            style="color: rgb(41, 98, 255); caret-color: rgb(41, 98, 255);"
+          ></i>
+        </div>
+        <div data-v-b9fde658 class="v-list-item__title">
+          <a data-v-b9fde658 class="btmBorderAnim2" style="color: inherit;">Getting started tips</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.v-card:not(.v-sheet--tile):not(.v-card--shaped) {
+  border-radius: 16px;
+}
+</style>
